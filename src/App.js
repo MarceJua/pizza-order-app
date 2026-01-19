a2609711fb3228a136a13db8f79f29f28775bd51
MarceJua <mjuarez2017ig@gmail.com>
2026-01-19 11:26:40 -0600

    Add initial HTML and React components for Padre Gino's application

---
+++ src/App.js
@@ -0,0 +1,21 @@
+const Pizza = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("p", {}, props.description),
+    ]); 
+}
+
+const App = () => {
+    return React.createElement("div",
+        {},
+        [
+            React.createElement("h1", {}, "Welcome to Padre Gino's!"), // To create and stamp out a reusable component instance, similar to creating a stamp that can be used multiple times
+            React.createElement(Pizza, {name: "Margherita", description: "Classic delight with 100% real mozzarella cheese"}), // Using the Pizza component inside the App component
+            React.createElement(Pizza, {name: "pepperoni", description: "A classic pepperoni pizza with mozzarella cheese"}), // Reusing the Pizza component
+            React.createElement(Pizza, {name: "Veggie", description: "Loaded with fresh vegetables and mozzarella cheese"}), // Reusing the Pizza component  
+        ]
+    )
+}
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container); //method is used to create a root for rendering a React application
+root.render(React.createElement(App));
