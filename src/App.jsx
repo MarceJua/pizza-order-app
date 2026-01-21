93904457969ed1dc908058e0086dc6db056ca813
MarceJua <mjuarez2017ig@gmail.com>
2026-01-21 16:39:02 -0600

    feat: create Order component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza.jsx";
+import Order from "./Order.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>Welcome to the Pizza App</h1>
-      <Pizza 
-        name="Pepperoni" 
-        description="A classic American taste! Relish the delectable flavor of Pepperoni, topped with extra cheese" 
-        image={"/public/pizzas/pepperoni.webp"} 
-      />
-      <Pizza 
-        name="Hawaiian" 
-        description="A tropical delight with ham and pineapple" 
-        image="/public/pizzas/hawaiian.webp" 
-      />
-      <Pizza 
-        name="Americano" 
-        description="A bold and spicy flavor with a kick" 
-        image="/public/pizzas/big_meat.webp" 
-      />
+      <Order />
     </div>
   );
 };
